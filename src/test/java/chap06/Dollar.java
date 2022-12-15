bd26c79738f4f23435310b78739113557f9ddbeb
ys200209 <lys200209@naver.com>
2022-12-15 23:22:07 +0900

    feat(tdd): Init TDD Sample Code

---
+++ src/test/java/chap06/Dollar.java
@@ -0,0 +1,11 @@
+package chap06;
+
+public class Dollar extends Money {
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
