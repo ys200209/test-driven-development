de690b80b6ce562ddedc184565094c39d9fe7c87
ys200209 <lys200209@naver.com>
2022-12-24 23:50:50 +0900

    feat(tdd): Init TDD Sample Code

--- src/test/java/chap28/TddTest.java
+++ src/test/java/chap28/TddTest.java
@@ -7,9 +7,36 @@ import org.junit.jupiter.api.Test;
 
 public class TddTest {
 
-    @DisplayName("MyDate 테스트")
+    /*@DisplayName("MyDate 테스트")
     @Test
     void testMyDate() {
         assertEquals(new MyDate("28.2.02"), new MyDate("1.3.02").yesterday());
+    }*/
+
+    /*@DisplayName("더하기 테스트")
+    @Test
+    void testSum() {
+        assertEquals(4, plus(3, 1));
+        assertEquals(7, plus(3, 4));
+    }
+
+    private int plus(int augend, int addend) {
+        return augend + addend;
+    }*/
+
+    @DisplayName("배열의 합 테스트")
+    @Test
+    void testSum() {
+        assertEquals(5, sum(new int[]{5}));
+    }
+
+    private int sum(int[] values) {
+        int sum = 0;
+
+        for (int number : values) {
+            sum += number;
+        }
+
+        return sum;
     }
 }
---
+++ src/test/java/chap29/TddTest.java
@@ -0,0 +1,23 @@
+package chap29;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TddTest {
+
+    @DisplayName("객체를 블랙박스 취급 테스트")
+    @Test
+    void testBlackBoxObject() {
+        // given
+        Contract contract = new Contract(); // status : Offered (default)
+
+        // when
+        contract.begin(); // status : Running
+
+        // then
+        assertEquals(Running.class, contract.status.class);
+    }
+
+}
