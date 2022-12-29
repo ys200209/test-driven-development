a2520a60641ec045fc64cef15038235dbd908e84
ys200209 <lys200209@naver.com>
2022-12-29 14:11:58 +0900

    feat(tdd): Init TDD Sample Code

--- src/test/java/chap32/TddTest.java
+++ src/test/java/chap32/TddTest.java
@@ -2,78 +2,82 @@ package chap32;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TddTest {
+    Triangle triangle;
+
+    @BeforeEach
+    public void init() {
+        triangle = new Triangle();
+    }
+
     @DisplayName("equilateral triangle 테스트")
     @Test
     void testEquilateralTriangle() {
         // given
         Triangle triangle = new Triangle();
 
-        // when
-        triangle.init(1, 1, 1);
-
         // then
-        assertEquals(triangle.result(), 1);
+        assertEquals(triangle.compare(1, 1, 1), 1);
     }
 
     @DisplayName("isosceles triangle 테스트")
     @Test
     void testIsoscelesTriangle() {
-        // given
-        Triangle triangle = new Triangle();
+        assertEquals(triangle.compare(2, 2, 3), 2);
+    }
 
-        // when
-        triangle.init(2, 2, 3);
-
-        // then
-        assertEquals(triangle.result(), 2);
+    @DisplayName("scalene triangle 테스트")
+    @Test
+    void testScaleneTriangle() {
+        assertEquals(triangle.compare(2, 3, 4), 3);
     }
 
     private class Triangle {
-        int sideA;
-        int sideB;
-        int sideC;
 
-        public void init(int sideA, int sideB, int sideC) {
-            this.sideA = sideA;
-            this.sideB = sideB;
-            this.sideC = sideC;
-        }
+        public int compare(int sideA, int sideB, int sideC) {
+            int[] sizes = sortSize(toArray(sideA, sideB, sideC));
+//            validateSizes(sizes);
+            int count = getEqualsSizeCount(sizes);
 
-        public int result() {
-            int count = compare();
-
-            return resultTriangle(count);
-        }
-
-        private int resultTriangle(int count) {
             if (count == 3) {
                 return 1;
-            } else if (count == 2) {
+            } else if (count == 1) {
                 return 2;
             } else {
                 return 3;
             }
         }
 
-        private int compare() {
+        private int getEqualsSizeCount(int[] sizes) {
             int count = 0;
 
-            count += compareSide(sideA, sideB);
-            count += compareSide(sideA, sideC);
-            count += compareSide(sideB, sideC);
-
+            for (int i = 0; i < sizes.length - 1; i++) {
+                for (int j = i + 1; j < sizes.length; j++) {
+                    count += equalSizeCount(sizes[i], sizes[j]);
+                }
+            }
             return count;
         }
 
-        private int compareSide(int side1, int side2) {
-            if (side1 == side2) {
+        private int equalSizeCount(int sizeA, int sizeB) {
+            if (sizeA == sizeB) {
                 return 1;
             }
             return 0;
+        }
+
+        private int[] toArray(int sideA, int sideB, int sideC) {
+            return new int[]{sideA, sideB, sideC};
+        }
+
+        private int[] sortSize(int[] sizes) {
+            Arrays.sort(sizes);
+            return sizes;
         }
     }
 }
